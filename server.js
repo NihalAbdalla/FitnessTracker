857b5f477330fb321877ebb50365f81f8dba5396
Nihal Abdalla <miz46769@gmail.com>
2021-07-09 00:28:11 -0400

    create server.js

--- server.js
+++ server.js
@@ -1,29 +1,48 @@
 const express = require("express");
-const mongojs = require("mongojs");
-
 const app = express();
+const mongojs = require("mongojs");
+const path = require("path");
 
 const databaseUrl = "fitness";
-const collections = ["workout"];
+const collections = ["workouts"];
 
-const db = mongojs(databaseUrl, workoutCollections);
+const db = mongojs(databaseUrl, collections);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
 db.on("error", error => {
   console.log("Database Error:", error);
 });
 
-app.get("/", (request, response) => {
-  res.send("");
+app.get("/", (req, res) => {
+  res.sendFile("Hello world"); 
 });
 
-app.get("/all", (request, response) => {
-  db.workout.find({}, (error, found) => {
+app.get("/all", (req, res) => {
+  db.workout.find({}, (err, found) => {
     if (err) {
-      console.log(error);
+      console.log(err);
     } else {
       res.json(found);
     }
   });
+});
+
+app.get("/find/:id", (req, res) => {
+  db.workout.findOne(
+    {
+      _id: mongojs.ObjectId(req.params.id)
+    },
+    (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    }
+  );
 });
 
 app.listen(3000, () => {
